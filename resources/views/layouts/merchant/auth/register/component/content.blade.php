725f820066e221617d62e26d8a61d96708d969d9
IaMGeorgious <iqbalabdulmajid02@gmail.com>
2024-09-13 09:02:53 +0700

    add(ui):login merchant and customer

---
+++ resources/views/layouts/merchant/auth/register/component/content.blade.php
@@ -0,0 +1,41 @@
+<body>
+    <main>
+      <section id="auth-hero">
+        <img src="img/sign-up-image.png" alt="Image Sign Up" class="background">
+        <div class="content">
+          <h2 class="title">Selangkah Lebih Dekat Dengan Impianmu</h2>
+          <p class="description">Sebuah layanan E-Learning gratis yang siap membantumu menjadi seorang ahli</p>
+        </div>
+      </section>
+      <section id="auth-form">
+        <a class="btn-back" href="/">
+          <i class="fa-solid fa-arrow-left"></i>
+          <p>Homepage</p>
+        </a>
+        <div class="header">
+          <h1>Sign Up</h1>
+          <p>Persiapkan diri untuk masa depan yang penuh dengan bintang</p>
+        </div>
+        <form action="" method="post">
+          <div class="input-group border-primary">
+            <label for="name">Name</label>
+            <input type="text" class="placeholder-primary" id="name" name="name" placeholder="Your name">
+          </div>
+          <div class="input-group border-primary">
+            <label for="email">Email</label>
+            <input type="email" class="placeholder-primary" id="email" name="email" placeholder="Your email">
+          </div>
+          <div class="input-group border-primary">
+            <label for="password">Password</label>
+            <input type="password" class="placeholder-primary" id="password" name="password" placeholder="Your password">
+          </div>
+          <div class="input-group border-primary">
+            <label for="password-confirmation">Password Confirmation</label>
+            <input type="password" class="placeholder-primary" id="password-confirmation" name="password-confirmation" placeholder="Your password confirmation">
+          </div>
+          <button type="submit" class="btn-secondary btn-submit">Sign Up</button>
+          <p class="text-center">Already have an account? <a href="signin" class="link">Sign In</a></p>
+        </form>
+      </section>
+    </main>
+  </body>
