9ff3594c367669625f7c828ff5f70707989d307f
guntur <fxguntur04@gmail.com>
2024-09-13 14:48:18 +0700

    fix(routes): fixing routes web.php

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@ require __DIR__ . '/merchant.php';
 // Memanggil rute untuk admin
 require __DIR__ . '/admin.php';
 
+// Memanggil rute untuk customer
+require __DIR__ . '/customer.php';
+
 // Home
 Route::get('/', function () {
     return view('layouts.customer.home.pages.index');
@@ -62,27 +65,5 @@ Route::get('/signup', function () {
     return view('layouts.customer.auth.register.pages.signup');
 });
 
-Route::get('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
-Route::post('/admin/login', [AdminAuthController::class, 'store'])->name('admin.login.store');
 
-Route::get('/customer/login', [CustomerAuthController::class, 'login'])->name('customer.login');
-Route::post('/customer/login', [CustomerAuthController::class, 'store'])->name('customer.login.store');
-
-Route::get('/merchant/login', [MerchantAuthController::class, 'login'])->name('merchant.login');
-Route::post('/merchant/login', [MerchantAuthController::class, 'store'])->name('merchant.login.store');
-
-Route::get('/admin/logout', [AdminAuthController::class, 'logoutAdmin'])->name('admin.logout');
-Route::get('/customer/logout', [CustomerAuthController::class, 'logoutCustomer'])->name('customer.logout');
-Route::get('/merchant/logout', [MerchantAuthController::class, 'logoutMerchant'])->name('merchant.logout');
-
-Route::middleware('auth:customer')->group(function () {
-    Route::get('/checkout', [CustomerPagesController::class, 'index'])->name('customer.checkout');
-});
-
-Route::middleware('auth:merchant')->group(function () {
-    Route::get('/merchant-panel', [MerchantPagesController::class, 'index'])->name('merchant.dashboard');
-});
-
-Route::middleware('auth:admin')->group(function () {
-    Route::get('/admin-panel', [AdminPagesController::class, 'index'])->name('admin.dashboard');
-});
+?>
